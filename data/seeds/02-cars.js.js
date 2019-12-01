10136d5103c754cec9494fd2e3f579b503102ff4
brit-starks <brittneytechiethomas@gmail.com>
2019-11-30 23:57:53 -0800

    Add transmission types

--- data/seeds/02-cars.js.js
+++ data/seeds/02-cars.js.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cars').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cars').insert([
+        { make: 'toyota', model: 'camry', milage: '90K', transmission: "automatic"},
+        { make: 'ford', model: 'focus', milage: '52K', transmission: "stick"},
+        { make: 'nissan', model: 'altima', milage: '37K', transmission: "automatic"},
+        { make: 'mazda', model: '6', milage: '5K', transmission: "stick"},
+        { make: 'toyota', model: 'corolla', milage: '72K', transmission: "stick"},
+        { make: 'chevy', model: 'corvette', milage: '30K', transmission: "stick"}
       ]);
     });
 };
