a091c15aa779e9e5bc67d7e42eefd432e3302eca
brit-starks <brittneytechiethomas@gmail.com>
2019-11-30 23:56:17 -0800

    Add minor fixes

--- helpers/carRouter.js
+++ helpers/carRouter.js
@@ -8,18 +8,30 @@ const carsdb = require('../data/db-config.js');
 router.use(express.json());
 
 router.get('/', (req, res) => {
-    // console.log(req.query);
-    carsdb('cars')
-        .then(cars => {
-            res.status(200).json(cars);
-        })
-        .catch(error => {
-            // log error to database
-            console.log(error);
-            res.status(500).json({
-                message: 'Error retrieving cars',
-            });
+  carsdb('cars')
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(error => {
+      console.log(error);
+        res.status(500).json({
+          message: 'Error retrieving cars',
         });
+    });
 });
+
+router.post('/', (req, res) => {
+	const newCar = req.body;
+    carsdb('cars').insert(newCar).into('cars')
+        .then(car => {
+					res.status(201).json(car)
+				})
+				.catch(err => {
+					res.status(500).json({
+						error: err,
+						message: 'failed to add car info'
+					})
+				})
+})
 
 module.exports = router;
--- seeds/01-cars.js
+++ seeds/01-cars.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('cars').insert([
         { make: 'toyota', model: 'camry', milage: '90K'},
-        { make: 'ford', model: 'focus', milage: '52K'},
+        { make: 'ford', model: 'focus', milage: '52K', },
         { make: 'nissan', model: 'altima', milage: '37K'},
         { make: 'mazda', model: '6', milage: '5K'},
         { make: 'toyota', model: 'corolla', milage: '72K'},
